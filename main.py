6e10197da2f5e9dc318ebb9c697a6df5c5f4776a
Weilia95 <weiliang95.chen@gmail.com>
2025-05-28 16:51:28 +0800

    常规修复

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 
 # sneaker_manager/main.py
 from app.ui import SneakerApp
-from app.database import init_db  # ✅ 导入初始化函数
+from app.database import init_db
 import customtkinter as ctk
 
 if __name__ == "__main__":
-    init_db()  # ✅ 在程序启动前初始化数据库
+    init_db()
     ctk.set_appearance_mode("dark")
     ctk.set_default_color_theme("blue")
     app = SneakerApp()
